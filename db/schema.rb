bf2e45cd9d8ba5f4eb2c848209bef38c910bd1d8
Damilare Kuye <kuyes2390@gmail.com>
2024-06-01 18:38:32 +0200

    .

--- db/schema.rb
+++ db/schema.rb
@@ -10,18 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_01_145206) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_01_155134) do
   create_table "restaurants", force: :cascade do |t|
     t.string "name"
     t.string "address"
+    t.string "phone_number"
     t.string "category"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "reviews", force: :cascade do |t|
-    t.text "content"
     t.integer "rating"
+    t.text "content"
     t.integer "restaurant_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
