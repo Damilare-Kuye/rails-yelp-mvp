6523d013a94f4d3830bceaade676dc1a6a8c17ae
Damilare Kuye <kuyes2390@gmail.com>
2024-06-02 15:04:49 +0200

    Seeding added

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,31 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+# db/seeds.rb
+
+puts "Cleaning database..."
+Review.destroy_all
+Restaurant.destroy_all
+
+puts "Creating restaurants..."
+dishoom = { name: "Dishoom", address: "7 Boundary St, London E2 7JE", category: "chinese" }
+pizza_east = { name: "Pizza East", address: "56A Shoreditch High St, London E1 6PQ", category: "belgian"}
+
+restaurants = [dishoom, pizza_east].map do |attributes|
+  restaurant = Restaurant.create!(attributes)
+  puts "Created #{restaurant.name}"
+  restaurant
+end
+
+puts "Creating reviews..."
+reviews = [
+  { rating: 5, content: "Very nice place",restaurant: restaurants[0] },
+  { rating: 4, content: "The food is amazing",restaurant: restaurants[1] }
+]
+
+reviews.each do |review_attributes|
+  review = Review.create!(review_attributes)
+  puts "Created review with rating #{review.rating} for #{review.restaurant.name}"
+end
+
+puts "Finished!"
