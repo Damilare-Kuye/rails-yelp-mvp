05c38ff57381c364b0bdd1fca8d57144320c92e3
Damilare Kuye <kuyes2390@gmail.com>
2024-06-02 17:43:38 +0200

    .

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,33 @@
 Rails.application.routes.draw do
+  get 'reviews/new'
+  get 'reviews/create'
+  get 'restaurants/index'
+  get 'restaurants/new'
+  get 'restaurants/show'
+  get 'restaurants/create'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
+
+  Rails.application.routes.draw do
+  get 'reviews/new'
+  get 'reviews/create'
+  get 'restaurants/index'
+  get 'restaurants/new'
+  get 'restaurants/show'
+  get 'restaurants/create'
+    resources :restaurants , only: [:index, :show, :new, :create] do
+
+      member do
+        get :review
+      end
+      
+    resources :reviews, only: [:new, :create]
+
+    end
+  end
 
   # Defines the root path route ("/")
   # root "posts#index"
