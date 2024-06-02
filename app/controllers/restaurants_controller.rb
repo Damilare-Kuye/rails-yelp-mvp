7f21b8a4cd998c32f2bbc4cb1dfc3aee0064e217
Damilare Kuye <kuyes2390@gmail.com>
2024-06-02 20:22:00 +0200

    .

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,13 +1,25 @@
 class RestaurantsController < ApplicationController
   def index
+    @restaurants = Restaurant.all
   end
 
   def new
+    @restaurant = Restaurant.new
   end
 
   def show
+    @restaurant = Restaurant.find(params[:id])
+    @reviews = @restaurant.reviews
   end
 
   def create
+    @restaurant = Restaurant.new(params[:restaurant])
+    @restaurant.save
+  end
+
+  private
+  
+  def restaurant_params
+    params.require(:restaurant).permit(:name, :address, :category)
   end
 end
--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,7 +1,18 @@
 class ReviewsController < ApplicationController
   def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = @restaurant.reviews.new
   end
 
   def create
+    @restaurant = Restaurant.find(params[:id])
+    @review = @restaurant.reviews.new(review_params)
+    @review.save
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
   end
 end
